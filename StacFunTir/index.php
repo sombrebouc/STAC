2c98124a868f0f692dc2d7f56a512d6a1b603761
Sombrebouc <gautier.chassang@gmail.com>
2020-07-05 18:17:38 +0200

    modif navbar

--- StacFunTir/index.php
+++ StacFunTir/index.php
@@ -3,9 +3,8 @@
 
 
 <!-- \\\\\ NAVIGATION BAR ///// -->
-
 <nav class="navbar fixed-top navbar-expand-md col-12 navbar-dark bg-dark p-2">
-    <a href="#" class="navbar-brand">Utilisateur</a>
+    <a href="#" class="navbar-brand"><?= $userlogin ?></a>
     <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -28,7 +27,7 @@
 <!-- =====\\= CONNEXION =//===== -->
 <div class="modal fade" id="modal-container-connexion" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 	<div class="modal-dialog" role="document">
-		<div class="modal-content">
+		<div class="modal-content bg-success">
 			<div class="modal-header">
 				<h5 class="modal-title" id="myModalLabel">Connexion</h5> 
 				<button type="button" class="close" data-dismiss="modal">
@@ -36,14 +35,14 @@
 				</button>
 			</div>
 			<div class="modal-body">
-                <form action="">
-                    <div class="inputBox">
+                <form action="GET">
+                    <div class="inputBox p-1">
                         <input type="text" name="" required="">
-                        <label for="">Utilisateur</label>
+                        <label class="m-right-2 text-lowercase" for="">Utilisateur</label>
                     </div>
-                    <div class="inputBox">
+                    <div class="inputBox p-2">
                         <input type="password" name="" required="">
-                        <label for=""">MdP</label>
+                        <label class="m-right-2 text-lowercase" for=""">Mot de passe</label>
                     </div>
                 </form>
 			</div>
@@ -62,7 +61,7 @@
 			<div class="modal-header">
 				<h5 class="modal-title" id="myModalLabel">Inscription</h5> 
 				<button type="button" class="close" data-dismiss="modal">
-					<span aria-hidden="true">×</span>
+					<span aria-hidden="true">X</span>
 				</button>
 			</div>
 			<div class="modal-body">
@@ -77,7 +76,6 @@
 </div>
 
 <!-- \\\\\ HEADER TITLE ///// -->
-
 <div class="container-fluid col-md-12 p-0">
     <div class="row">
         <header class="headerbanner bg-dark">
@@ -85,49 +83,48 @@
         </header>
     </div>
 
-    <!-- \\\\\ CONTENU INFORMATIONS ///// -->
-    <div class="container-fluid">
-        <div class="row">
-            <div class="containerInfosFftir p-3 bg-danger text-dark col-md-8 rounded">
-                <h1 class="titlebody m-1 p-1 text-uppercase bg-info rounded">FFTir Infos</h1>
-                <h3 class="p-3">Le Tir Sportif</h3>
-                <div class="p-3">
-                    <p>
-                        Vous venez d'adhérer à la Fédération Française de Tir ; soyez le bienvenu dans ce sport qui,
-                        nous en
-                        sommes persuadés, vous apportera beaucoup de plaisir ! Que vous vous dirigiez vers du loisir ou
-                        de la
-                        compétition, la pratique du Tir sportif nécessite de votre part le respect de règles intangibles
-                        de
-                        sécurité et la connaissance des principaux fondamentaux.
-                        Cette présente rubrique a pour objet de vous guider dans vos premiers pas de tireur et de faire
-                        connaissance avec La Fédération Française de Tir.
-                        Vous y trouverez des informations réglementaires comme les règles de sécurité édictées par la
-                        FFTir à
-                        respecter en toutes circonstances et le dispositif réglementaire du carnet de tir. Vous pourrez
-                        également découvrir les différentes disciplines proposées par la FFTir qu'il vous sera possible
-                        de
-                        pratiquer et ce, en fonction des installations de l'association dont vous venez de devenir
-                        adhérent.
-                        Vous aurez accès à diverses informations concernant le matériel que vous pourrez utiliser ainsi
-                        que les
-                        principes élémentaires et incontournables de la technique de base. Vous pourrez également
-                        découvrir
-                        l'histoire et l'organisation de la Fédération Française de Tir et un lexique destiné à vous
-                        familiariser
-                        avec le jargon du Tir sportif.
-                        Vous faites maintenant partie de la Fédération Française de Tir. À ce titre et en toutes
-                        circonstances,
-                        vous vous devez d'en respecter l'éthique et les valeurs. Pour en savoir plus, n'hésitez pas à
-                        télécharger le manuel d'initiation du tireur sportif.
-                    </p>
-                    <p class="p-3">Le Président de la FFTir</p>
-                    <p class="p-3">Philippe Crochard</p>
-                </div>
+<!-- \\\\\ CONTENU INFORMATIONS ///// -->
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="containerInfosFftir p-3 m-3 bg-success text-light col-md-8 justify-content-center rounded">
+            <h1 class="titlebody m-1 p-1 text-uppercase bg-dark text-light rounded">FFTir Infos</h1>
+            <h3 class="p-3">Le Tir Sportif</h3>
+            <div class="p-3">
+                <p>
+                    Vous venez d'adhérer à la Fédération Française de Tir ; soyez le bienvenu dans ce sport qui,
+                    nous en
+                    sommes persuadés, vous apportera beaucoup de plaisir ! Que vous vous dirigiez vers du loisir ou
+                    de la
+                    compétition, la pratique du Tir sportif nécessite de votre part le respect de règles intangibles
+                    de
+                    sécurité et la connaissance des principaux fondamentaux.
+                    Cette présente rubrique a pour objet de vous guider dans vos premiers pas de tireur et de faire
+                    connaissance avec La Fédération Française de Tir.
+                    Vous y trouverez des informations réglementaires comme les règles de sécurité édictées par la
+                    FFTir à
+                    respecter en toutes circonstances et le dispositif réglementaire du carnet de tir. Vous pourrez
+                    également découvrir les différentes disciplines proposées par la FFTir qu'il vous sera possible
+                    de
+                    pratiquer et ce, en fonction des installations de l'association dont vous venez de devenir
+                    adhérent.
+                    Vous aurez accès à diverses informations concernant le matériel que vous pourrez utiliser ainsi
+                    que les
+                    principes élémentaires et incontournables de la technique de base. Vous pourrez également
+                    découvrir
+                    l'histoire et l'organisation de la Fédération Française de Tir et un lexique destiné à vous
+                    familiariser
+                    avec le jargon du Tir sportif.
+                    Vous faites maintenant partie de la Fédération Française de Tir. À ce titre et en toutes
+                    circonstances,
+                    vous vous devez d'en respecter l'éthique et les valeurs. Pour en savoir plus, n'hésitez pas à
+                    télécharger le manuel d'initiation du tireur sportif.
+                </p>
+                <p class="p-3">Le Président de la FFTir</p>
+                <p class="p-3">Philippe Crochard</p>
             </div>
-
         </div>
     </div>
+</div>
 
 </div>
 
