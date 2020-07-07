580a340251568f5107dc63240ca188d67c837fe5
Sombrebouc <gautier.chassang@gmail.com>
2020-07-07 23:35:21 +0200

    modif css connexion

--- StacFunTir/connexion.php
+++ StacFunTir/connexion.php
@@ -11,21 +11,23 @@ setcookie('$licence', '$password', time() + 24*3600, null, null, false, true);
 ?>
 
 <!-- =====\\= CONNEXION =//===== -->
-<div class="connectForm">
+<div class="container-fluid col-4">
 	<div class="row">
-		<div class="container-fluid m-auto bg-secondary rounded w-auto p-2">
-	<form action="GET">
-		<div class="inputBox">
+	<form  class="formContainer" action="GET">
+		<div class="inputBox p-md-2">
+			<label for="licence">N° de licence FFTIR</label>
+			<div>
 			<input type="text" name="licence" required="">
-			<label class="m-right-2 text-lowercase" for="">N° de licence FFTIR</label>
+			</div>
 		</div>
-		<div class="inputBox">
+		<div class="inputBox p-md-2">
+			<label for="password">Mot de passe</label>
+			<div>
 			<input type="password" name="password" required="">
-			<label class="m-right-2 text-lowercase" for="">Mot de passe</label>
+			</div>
         </div>
 			<button href="#" type="submit" class="btn btn-dark">Valider</button>
 		</div>
 	</form>
-	</div>
 	</div>
 </div>
