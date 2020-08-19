0a62ff1a04ef4d9c2f266ff8797c06631bc85d12
Chassang <gautier.chassang@gmail.com>
2020-08-19 16:13:32 +0200

    modif bg et ameliorations des pages

--- StacFunTir/index.php
+++ StacFunTir/index.php
@@ -1,16 +1,3 @@
 <?php
 	include_once dirname(__FILE__).'\views\includes\header.php';
-?>
-
-<div class="container-fluid col-md-12 p-0">
-    <div class="row">
-        <header class="headerbanner text-center col-md-10">
-            <img class="logoSTAC img-fluid expand-md-6" src="\assets\img\STAC_logo-1000x512.png"
-                alt="logoSTAC">
-        </header>
-    </div>
-</div>
-
-<?php
 	include_once dirname(__FILE__).'\views\includes\footer.php';
-?>
