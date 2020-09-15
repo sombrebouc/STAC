8be64885f7b265aa13ba9402eaea52ee184b318f
GauT.C <gautier.chassang@gmail.com>
2020-09-15 17:24:17 +0200

    creatioin du model Games en lien avec la nouvelle table gamesession

---
+++ StacFunTir/models/Games.php
@@ -0,0 +1,68 @@
+<?php
+    require_once dirname(__FILE__).'\..\utils\Database.php';
+
+    class Game{
+        private $id;
+        private $date;
+        private $id_games;
+        private $db;
+
+        // Méthode magique appelée lors du chargement "new User()"
+        public function __construct($_id=0,$_date='',$id_games=''){
+            $this->db = Databases::getInstance();
+        // hydratation 
+            $this->id = $_id;
+            $this->date = $_date;
+            $this->id_games = $id_games;
+        }
+
+        public function __get($attr){
+            return $this->$attr;
+        }
+
+        public function __set($attr, $value){
+            $this->$attr = $value;
+        }
+        
+        public function create(){
+			$insertGames_sql = 'INSERT INTO `gamesession` (`id`, `date`, `id_games`) VALUES (:id, :date, :id_games);';
+            $gamesStatement = $this->db->prepare($insertGames_sql);
+			$gamesStatement->bindValue(':id', $this->id, PDO::PARAM_INT);
+			$gamesStatement->bindValue(':date', $this->date, PDO::PARAM_STR);
+            $gamesStatement->bindValue(':id_games', $this->firstname, PDO::PARAM_STR);
+            return $gamesStatement->execute();
+        }
+
+        /**
+         * retour liste des users enregistré
+         * @return array
+         */
+
+		public function readAll(){
+            $listGames_sql = 'SELECT `id`,`date`, `id_games` FROM `gamesession`;';
+            $gamesStatement = $this->db->query($listGames_sql);
+            $listGames = [];
+            if ($gamesStatement instanceof PDOstatement){
+                $listGames = $gamesStatement->fetchAll(PDO::FETCH_OBJ);
+            }
+            return $listGames;
+        }
+		public function readSingle(){
+            $gameInfos_sql = 'SELECT `date`, `id_games` FROM `gamesession` WHERE `id`=:id';
+            $gameStatement = $this->db->prepare($gameInfos_sql);
+            $gameStatement->bindValue(':id', $this->id,PDO::PARAM_INT);
+            $gameInfos = null;
+            if ($gameStatement->execute()){
+                $gameInfos = $gameStatement->fetch(PDO::FETCH_OBJ);
+            }
+            return $gameInfos;
+        }
+        
+		// public function delete(){
+        //     $usersDelete_sql = 'DELETE FROM `gamesession` WHERE `id`=:id;';
+        //     $usersDelete = $this->db->prepare($usersDelete_sql);
+        //     $usersDelete->bindValue(':id', $this->id,PDO::PARAM_INT);
+        //     return $usersDelete->execute();
+        // }   
+        
+    }
--- StacFunTir/views/gameViews/TargetChoice.php
+++ StacFunTir/views/gameViews/TargetChoice.php
@@ -1,15 +1,17 @@
 <div class="container-fluid targetCard col-xs-10 col-sm-10 col-md-6 mt-5 m-auto">
     <div class="row">
         <div class="targetHeaderPix"></div>
-        <div class="containerFormTarget shadow-sm ml-auto mt-1 mb-2 bg-light">
+        <div class="containerFormTarget shadow-sm ml-auto mt-1 mb-2 bg-dark">
             <form action="" method="post">
                 <div>
-                    <h3 class="text-uppercase text-light text-center">Ciblerie</h3>
+                    <h4 class="text-uppercase text-light text-center">Ciblerie</h4>
                 </div>
                 <div class="row">
-                    <input class="targetNbContainer rounded p-2 mb-3 m-auto col-8" type="text" name="targetNb" value="" required="">
-                    <input type="submit" class="btn btn-success p-0 m-auto col-8"
-                        href="\..\..\controllers\gameControllers\SelectingListController.php" value="VALIDER LA LISTE">
+                    <input class="targetNbContainer bg-light rounded p-2 mt-0 m-auto col-2" type="text" name="targetNb" value="" required="">
+                </div>
+                <div class="row">
+                    <input type="submit" class="btn btn-success p-2 mt-2 m-auto col-8"
+                        href="\..\..\controllers\gameControllers\SelectingListController.php" value="SUIVANT">
                 </div>
             </form>
         </div>
